165c54bc71418a839aaa007c8285da067623dbcb
skama1000 <skamchristo@gmail.com>
2024-12-08 12:35:06 +0200

    Update bruteforce.cmd

--- Source Code/bruteforce.cmd
+++ Source Code/bruteforce.cmd
@@ -395,7 +395,9 @@
 			if %%a==SSID (
 				set /a keynumber=!keynumber! + 1
 				set current_ssid=%%d
-
+				if "!current_ssid!"=="" (
+				set "current_ssid=Hidden_Network"
+				)
 				call :character_finder_2 "!current_ssid!"
 
 			)
